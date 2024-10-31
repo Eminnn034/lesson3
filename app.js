6e8c93ecfa21c2204df629d88f7bd5606b4082ad
Eminnn034 <emin.seyid97@gmail.com>
2024-11-01 02:04:38 +0400

    homework3

---
+++ app.js
@@ -0,0 +1,10 @@
+let user = prompt("Gelecek heyatinizda ne dusunursuz")
+console.log(user.length);
+console.log(user.replace("t"),("####"));
+console.log(user.startsWith ("") );
+console.log(user. endsWith(""));
+console.log(user.indexOf ("e"));
+
+
+
+
